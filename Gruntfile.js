297ed39f0daca53c9b0baddd4ba0d0f54c255b1d
Tarun Chinmai <tchinmai7@gmail.com>
2016-11-30 12:51:50 +0530

    fixed Readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function (grunt) {
     },
     open: {
       server: {
-        url: 'http://localhost:<%= express.options.port %>'
+        url: 'http://0.0.0.0:<%= express.options.port %>'
       }
     },
     watch: {
@@ -193,7 +193,7 @@ module.exports = function (grunt) {
     'node-inspector': {
       custom: {
         options: {
-          'web-host': 'localhost'
+          'web-host': '0.0.0.0'
         }
       }
     },
@@ -215,7 +215,7 @@ module.exports = function (grunt) {
             // opens browser on initial server start
             nodemon.on('config:update', function () {
               setTimeout(function () {
-                require('open')('http://localhost:8080/debug?port=5858');
+                require('open')('http://0.0.0.0:8080/debug?port=5858');
               }, 500);
             });
           }
--- README.md
+++ README.md
@@ -1,23 +1,3 @@
-# PaizaQA
-
-PaizaQA is a Open Source QA service(like StackOverflow) using MEAN stack.
-
-This project was generated with the [Angular Full-Stack Generator](https://github.com/DaftMonk/generator-angular-fullstack) version 3.3.0.
-
-## Blog article
-The following blog article explains how to build the QA service using MEAN stack.
-
-English: [Building a QA web service in an hour - MEAN stack development(3)](http://engineering.paiza.io/entry/2016/03/10/115345)
-
-Japanese: [Webサービスを作りたい人に最適、たった1時間でJSベースのQAサイトを作る方法 - MEANスタック開発(3)](http://paiza.hatenablog.com/entry/meanstack_howto_3)
-
-
-
-## Demo
-[http://collab.herokuapp.com](http://collab.herokuapp.com)
-
-
-## Getting Started
 
 ### Prerequisites
 
